3b25c072c8b5aea0eac19dad40832ceb0052af37
root <you@example.com>
2018-05-24 14:40:41 +0800

    1

--- functions/export-path.sh
+++ functions/export-path.sh
@@ -3,4 +3,4 @@
 
 #Notice that PS1 will be overide by .bashrc
 #export PS1="\[\e[31;1m\]\u @ \[\e[34;1m\]\h \[\e[36;1m\]\w \[\e[39;1m\]#"
-export PATH=$PATH:/root/repo-github/bash-scripts/commands
+export PATH=$PATH:/root/repo-github/bash-scripts/commands:/root/repo-github/bin
